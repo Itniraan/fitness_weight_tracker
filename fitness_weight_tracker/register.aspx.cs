da5ef2a30cc37d1a645d11c229f0bc9d8c27a3a4
Itniraan <blakemurdo@gmail.com>
2015-07-22 13:44:39 -0400

    Register page linked up to DB, register functional

--- fitness_weight_tracker/register.aspx.cs
+++ fitness_weight_tracker/register.aspx.cs
@@ -5,6 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// Identity Package References
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+
 namespace fitness_weight_tracker
 {
     public partial class register : System.Web.UI.Page
@@ -12,6 +18,29 @@ namespace fitness_weight_tracker
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Default UserStore constructor uses the default connection string named: DefaultConnection
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var user = new IdentityUser() { UserName = txtUserName.Text };
+            IdentityResult result = manager.Create(user, txtPassword.Text);
+
+            if (result.Succeeded)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+                Response.Redirect("/admin/main-menu.aspx");
+            }
+            else
+            {
+                lblStatus.Text = result.Errors.FirstOrDefault();
+                lblStatus.CssClass = "label label-danger";
+            }
         }
     }
 }
